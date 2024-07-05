114d92a5b3a98325396a70b046196ecb5613f232
hadapari <davs.richard1@gmail.com>
2024-07-05 14:33:03 +0300

    add outline on focus input in contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,63 +1,66 @@
 import { useForm } from "react-hook-form";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPaperPlane, faSpinner } from "@fortawesome/free-solid-svg-icons";
-import emailjs from '@emailjs/browser';
-import { useState } from "react";
-import { ToastContainer, toast } from 'react-toastify';
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
+import { Link } from "react-scroll";
 
 export type contactFormData = {
   name: string;
   email: string;
   message: string;
-}
-
+};
 
 const Contact = () => {
-  const [isSending, setIsSending] = useState(false)
-
+  const [isSending, setIsSending] = useState(false);
+  const inputNameRef = useRef(null);
 
   const {
     register,
     formState: { errors },
     handleSubmit,
-    reset
+    reset,
   } = useForm<contactFormData>();
 
-  
-
-const SERVICE_ID = import.meta.env.VITE_SERVICE_ID
-const TEMPLATE_ID =import.meta.env.VITE_TEMPLATE_ID
-const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
+  const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+  const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+  const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
   const onSubmit = handleSubmit((data) => {
     const emailData = {
-        from_name: data.name,
-        from_email: data.email,
-        message: data.message
-    }
+      from_name: data.name,
+      from_email: data.email,
+      message: data.message,
+    };
 
-    setIsSending(true)
+    setIsSending(true);
 
-    emailjs.send(SERVICE_ID, TEMPLATE_ID, emailData, PUBLIC_KEY)
-      .then(() => {
-            reset()
-            setIsSending(false)
-            toast.success("Message sent successfully ! I'll be in touch shortly", {
-              position: toast.POSITION.BOTTOM_RIGHT
-            });
-      }, (error) => {
-          console.log(error.text);
-          setIsSending(false)
-          toast.error("An error has occurred, please resend the message", {
-            position: toast.POSITION.BOTTOM_RIGHT
-          });
-      });
+    emailjs.send(SERVICE_ID, TEMPLATE_ID, emailData, PUBLIC_KEY).then(
+      () => {
+        reset();
+        setIsSending(false);
+        toast.success("Message sent successfully ! I'll be in touch shortly", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      },
+      (error) => {
+        console.log(error.text);
+        setIsSending(false);
+        toast.error("An error has occurred, please resend the message", {
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      }
+    );
   });
 
   return (
-    <section id="contact" className="px-4 sm:px-14 xl:px-40 py-14 bg-secondary text-white flex flex-col lg:flex-row">
+    <section
+      id="contact"
+      className="px-4 sm:px-14 xl:px-40 py-14 bg-secondary text-white flex flex-col lg:flex-row"
+    >
       <div className="lg:size-1/2 flex flex-col gap-7">
         <p className="text-2xl">
           Let' s make something amazing <br />
@@ -65,7 +68,22 @@ const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
         </p>
         <p className="text-2xl mb-16">
           Start by{" "}
-          <label className="text-teal border-b-2 border-teal cursor-pointer" htmlFor="name">saying hi</label>
+          <Link
+            to="contact"
+            className="text-teal border-b-2 border-teal cursor-pointer"
+            spy={true}
+            smooth={true}
+            offset={-90}
+            duration={500}
+            onClick={() => inputNameRef?.current?.focus()}
+          >
+            {/* <label
+              className="text-teal border-b-2 border-teal cursor-pointer"
+              htmlFor="name"
+            > */}
+            saying hi
+            {/* </label> */}
+          </Link>
         </p>
       </div>
       <div className="lg:size-1/2">
@@ -75,8 +93,9 @@ const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
               id="name"
               type="text"
               placeholder="name"
-              className="input bg-white w-full p-3 rounded text-black"
+              className="input bg-white w-full p-3 rounded text-black focus:outline focus:outline-teal-600"
               {...register("name", { required: "* This field is required" })}
+              ref={inputNameRef}
             />
             {errors.name && (
               <span className="text-sm text-red-500">
@@ -88,7 +107,7 @@ const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
             <input
               type="email"
               placeholder="email"
-              className="input bg-white w-full p-3 rounded text-black"
+              className="input bg-white w-full p-3 rounded text-black focus:outline focus:outline-teal-600"
               {...register("email", { required: "* This field is required" })}
             />
             {errors.email && (
@@ -102,7 +121,7 @@ const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
               placeholder="message"
               cols={30}
               rows={10}
-              className="textarea bg-white w-full p-3 rounded text-black"
+              className="textarea bg-white w-full p-3 rounded text-black focus:outline focus:outline-teal-600"
               {...register("message", { required: "* This field is required" })}
             ></textarea>
             {errors.message && (
@@ -111,8 +130,15 @@ const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY
               </span>
             )}
           </div>
-          <button type="submit" className="btn rounded hover:disabled:opacity-100 hover:opacity-80" disabled={isSending}>
-            <FontAwesomeIcon icon={isSending?faSpinner:faPaperPlane} className={`mr-2 ${isSending?`animate-spin`:``}`}/>
+          <button
+            type="submit"
+            className="btn rounded hover:disabled:opacity-100 hover:opacity-80"
+            disabled={isSending}
+          >
+            <FontAwesomeIcon
+              icon={isSending ? faSpinner : faPaperPlane}
+              className={`mr-2 ${isSending ? `animate-spin` : ``}`}
+            />
             Submit
           </button>
           <ToastContainer />
