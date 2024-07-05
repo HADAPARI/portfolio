218c31f84a8292f8e1a06a7a8b1db6a36f94dee5
hadapari <davs.richard1@gmail.com>
2024-07-05 14:13:46 +0300

    shadow on hover tech

--- src/components/Techs.tsx
+++ src/components/Techs.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const Techs = (props: any) => {
   return (
-    <div className="tech bg-primary rounded-md flex flex-col items-center mt-10 p-7 gap-3 w-72 h-80">
+    <div className="tech bg-primary rounded-md flex flex-col items-center mt-10 p-7 gap-3 w-72 h-80 hover:shadow-md hover:shadow-white/5">
       <div className="text-8xl text-teal icon flex justify-center w-24">
         {props.icon}
       </div>
